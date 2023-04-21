00de60c6780fa97ba5d519631360886daeeb6e98
Gulshan <gulshankumar.mailid01@gmail.com>
2023-04-21 18:06:29 +0530

    Add project files.

---
+++ SingnalRKeeda/Controllers/ServerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SingnalRKeeda.Models;
+
+namespace SingnalRKeeda.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly IHubContext<NotificationHub>? _notificationHub;
+
+        public ServerController(IHubContext<NotificationHub>? notificationHub)
+        {
+            _notificationHub = notificationHub;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Notification model)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            await _notificationHub.Clients.All.SendAsync("ReceiveMsg", model.Message);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            return View();
+        }
+    }
+}
---
+++ SingnalRKeeda/Models/Notification.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SingnalRKeeda.Models
+{
+    public class Notification
+    {
+        public string? Message { get; set; }
+    }
+}
